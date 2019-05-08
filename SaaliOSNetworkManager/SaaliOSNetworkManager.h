e073571ea35387b07adea8f15280322d68554649
Sahana Rao <sahana@saal.ai>
2019-05-08 11:19:33 +0400

    Initial Commit

---
+++ SaaliOSNetworkManager/SaaliOSNetworkManager.h
@@ -0,0 +1,19 @@
+//
+//  SaaliOSNetworkManager.h
+//  SaaliOSNetworkManager
+//
+//  Created by Sahana Rao on 5/8/19.
+//  Copyright © 2019 Sahana Rao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SaaliOSNetworkManager.
+FOUNDATION_EXPORT double SaaliOSNetworkManagerVersionNumber;
+
+//! Project version string for SaaliOSNetworkManager.
+FOUNDATION_EXPORT const unsigned char SaaliOSNetworkManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SaaliOSNetworkManager/PublicHeader.h>
+
+
